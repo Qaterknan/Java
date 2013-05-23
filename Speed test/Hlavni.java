e05dad390c7850a2e686fbf3f5e379da5efe1938
Izzet <izzetStMr@gmail.com>
2013-05-23 10:43:19 +0200

    optimalizace

--- Speed test/Hlavni.java
+++ Speed test/Hlavni.java
@@ -1,21 +1,24 @@
 import java.util.Date;
+import java.util.ArrayList;
 
 class Hlavni {
 	public static void main(String args[]){
 		long startCas = new Date().getTime();
+		ArrayList pole = new ArrayList();
 		boolean jePrvocislo = false;
-		for(int i = 0;i < 1000;i++){
+		for(int i = 0;i < 100000;i++){
 			jePrvocislo = true;
-			for(int j = i-1;j > 1;j--){
+			for(int j = (int) Math.floor(Math.sqrt(i));j > 1;j--){
 				if(i%j == 0){
 					jePrvocislo = false;
 				}
 			}
 			if(jePrvocislo){
-				System.out.println(i);
+				pole.add(i);
 			}
 		}
 		long endCas = new Date().getTime();
+		System.out.println(pole.toString());
 		System.out.println("Cas vypoctu: ");
 		System.out.println(endCas-startCas);
 	}
