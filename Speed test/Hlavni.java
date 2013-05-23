760dd64cb65767500bfb0c954be3081ed6bd7095
Izzet <izzetStMr@gmail.com>
2013-05-23 10:21:29 +0200

    Synch

--- Speed test/Hlavni.java
+++ Speed test/Hlavni.java
@@ -4,7 +4,7 @@ class Hlavni {
 	public static void main(String args[]){
 		long startCas = new Date().getTime();
 		boolean jePrvocislo = false;
-		for(int i = 0;i < 10;i++){
+		for(int i = 0;i < 1000;i++){
 			jePrvocislo = true;
 			for(int j = i-1;j > 1;j--){
 				if(i%j == 0){
