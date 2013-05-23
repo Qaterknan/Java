cda74d652b982e6fd022e4dd0a951481eabc7fca
Izzet <izzetStMr@gmail.com>
2013-05-23 20:19:03 +0200

    náčrt eventhandleru

---
+++ eventhandler pokusy/Hlavni.java
@@ -0,0 +1,18 @@
+import javax.swing.JFrame;
+
+public class Hlavni extends JFrame  {
+	
+	protected Eventhandler eventhandler = new Eventhandler(this);
+	
+	public Hlavni (){
+		super("Eventhandler");
+		setSize(300,300);
+		setLocation(100,100);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setVisible(true);
+	}
+	
+	public static void main (String args[]){
+		new Hlavni();
+	}
+}
