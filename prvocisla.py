c34b185cc453c405378f71e2e39e956516276581
Jirka Balhar <balhar.j@gmail.com>
2013-05-23 10:10:56 +0200

    prvocisla

---
+++ prvocisla.py
@@ -0,0 +1,20 @@
+from math import sqrt
+
+primes = [2]
+maxPrime = 100000
+
+is_prime = True
+i = 3
+while i < maxPrime:
+	i+=2;
+	is_prime = True
+	sq = sqrt(i)
+	for p in primes:
+		if p > sq:
+			break
+		if i % p == 0:
+			is_prime = False
+	if is_prime:
+		primes.append(i)
+
+print(primes)
